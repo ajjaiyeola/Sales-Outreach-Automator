1f0712cc8123d64513450abd19fd3ea77e95eff7
ajjaiyeola <aj@radiusnetworks.com>
2017-11-04 19:13:21 -0400

    Create inboxscanner.js

---
+++ inboxscanner.js
@@ -0,0 +1,28 @@
+  function getMail(){
+  var thread = GmailApp.getInboxThreads(0,100);
+  var sheet = SpreadsheetApp.getActiveSheet();
+  //scan sheet to see if email is there
+  var startRow = 1;  
+  var numRows = 15;   // Number of rows to process
+  var dataRange = sheet.getRange(startRow, 1, numRows, 10); // Start row 3 column 10, and stop row 40, column 50
+  var data = dataRange.getValues();  
+  for (var i = 0; i < thread.length; ++i){  
+     var message = thread[i].getMessages();
+     var lastOf = message.length - 1;
+     var currentMessage = message[lastOf];
+     var subject = currentMessage.getSubject();
+     var target = "Radius Networks Proximity Solutions"
+     if(subject.indexOf(target) !== -1) {
+        var sender = currentMessage.getFrom().replace(/^.+<([^>]+)>$/, "$1");
+          //if(sender !== "aj@radiusnetworks.com"){        
+            for(var b = 0; b < data.length; ++b){
+              var row = data[b];
+              if(row[4] == sender){
+                sheet.getRange(b + 1, 6).setValue("yes");   
+            }  
+            }       
+         // }   
+     }
+   }
+  }  
+   
